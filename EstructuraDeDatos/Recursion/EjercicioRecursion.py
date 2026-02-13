8bfdfb9c48cd0eee58d85e774d54ecfb61930285
CrispeX <crispeticasescobartaborda08@gmail.com>
2026-02-13 12:36:17 +0000

    Ejercicios Binarios

--- EstructuraDeDatos/Recursion/EjercicioRecursion.py
+++ EstructuraDeDatos/Recursion/EjercicioRecursion.py
@@ -91,7 +91,7 @@ print(es_palindrome(p2))"""
 
 #Implemente una funcion recursiva que reverse una lista
 
-def reversed_list(l):
+"""def reversed_list(l):
     if len(l) == 1:
         return l
     return [l[-1]]+reversed_list(l[:-1])
@@ -108,3 +108,37 @@ def reversed_list2(l, index=0, lr=[]):
 l = [4,5,6,7,111]
 print(l)
 print(reversed_list(l))
+"""
+
+#Implemente una funcion recursiva
+"""def busqueda_binaria(l, obj, i_low=-1, i_high=1):
+    if i_low == -1 and i_high == 1:
+        i_low = 0
+        i_high = len(l)-1
+    print("i_low", i_low)
+    print("i_hight", i_high)
+    i_med = (i_low+i_high)//2
+    print(i_low, i_med, i_high)
+
+    if l[i_med] == obj:
+        return i_med
+    
+    if i_low > i_high:
+      return "El objeto no fue encontrado"
+
+    if obj < l[i_med]:
+        #busqueda en la primera mitad
+        print("obj < l[i_med]")
+        return busqueda_binaria(l, obj, i_low, i_med-1)
+    elif obj > l[i_med]:
+      #busqueda en la segunda mitad
+      return busqueda_binaria(l, obj, i_med+1, i_high)
+
+
+l = [1,2,3,3,3,5,6,7,8,33,444,222,4,4545]
+v1 = 3
+v2 = 4
+v3 = 8
+print(f"El valor {v1} esta en la posicion {busqueda_binaria(l, v1)}")
+print(f"El valor {v2} esta en la posicion {busqueda_binaria(l, v2)}")
+print(f"El valor {v3} esta en la posicion {busqueda_binaria(l, v3)}")"""
