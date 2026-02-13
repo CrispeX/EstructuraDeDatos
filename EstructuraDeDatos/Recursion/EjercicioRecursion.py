57b254d486431111c4df5b39a261511458cead10
CrispeX <crispeticasescobartaborda08@gmail.com>
2026-02-13 12:12:12 +0000

    Ejercicios recursion

--- EstructuraDeDatos/Recursion/EjercicioRecursion.py
+++ EstructuraDeDatos/Recursion/EjercicioRecursion.py
@@ -1,7 +1,7 @@
 #Ejemplos:
 
 
-def abriruñeca(nummuñe):
+"""def abriruñeca(nummuñe):
   if nummuñe == 1:
     return print("Proceso finalizado:" + str(nummuñe))
   abriruñeca(nummuñe - 1)
@@ -23,7 +23,7 @@ def digitos(n:int):
   return 1+digitos(n//10)
 digitos(-1)
 
-
+#Reversar palabra
 def reversed_word(p):
   if len(p) == 1:
     return p
@@ -35,3 +35,76 @@ print(p[-1])
 print(p[:-1])
 print(p[::-1])
 
+def reversed_word(p, index = 0):
+  if len(p) == 1:
+    return p
+  return p[-1] + reversed_word(p[:-1])
+reversed_word("mesa")
+
+p = "mesa"
+print(p[-1])
+print(p[:-1])
+print(p[::-1])
+
+def reversed_word3(p, index = 0, pr = ""):
+  if index == len(p):
+    return pr
+  return reversed_word3(p, index + 1, p[index]+pr) 
+pq = "mesa"
+print(reversed_word3(pq))
+
+"""
+#Implemente una funcion recursiva que valide si una palabra es palindrome, Retornando True o False
+"""def es_palindrome(p, index1=-1, index2=-1):
+  if index1 == -1 and index2 == -1:
+    index1 = 0
+    index2 = len(p)-1
+  
+  if index1 == index2 or index2 < index1:
+    return True
+  
+  if p[index1] == p[index2]:
+    return es_palindrome(p, index1+1, index2-1)
+  
+  else:
+    return False
+
+p1 = "rotor"
+p2 = "mierda"
+p3 = "massam"
+print(es_palindrome(p1))
+print(es_palindrome(p2))
+print(es_palindrome(p3))
+
+def es_palindrome(p):
+  if len(p) == 1:
+    return True
+  if p[-1] == p[0]:
+    return es_palindrome(p[1:-1])
+  else:
+    return False
+
+p1 = "rotor"
+p2 = "mierda"
+print(es_palindrome(p1))
+print(es_palindrome(p2))"""
+
+#Implemente una funcion recursiva que reverse una lista
+
+def reversed_list(l):
+    if len(l) == 1:
+        return l
+    return [l[-1]]+reversed_list(l[:-1])
+
+l = [4,5,6,7,111]
+print(l)
+print(reversed_list(l))
+
+def reversed_list2(l, index=0, lr=[]):
+    if index == len(l):
+        return lr
+    return reversed_list2(l, index+1, [l[index]]+lr)
+
+l = [4,5,6,7,111]
+print(l)
+print(reversed_list(l))
