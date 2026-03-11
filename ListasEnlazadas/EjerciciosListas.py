f9ae7b41217ac018cc21b49dfe2b418f2691ef0b
CrispeX <crispeticasescobartaborda08@gmail.com>
2026-03-11 12:27:34 +0000

    Mas ejercicios

--- ListasEnlazadas/EjerciciosListas.py
+++ ListasEnlazadas/EjerciciosListas.py
@@ -16,3 +16,27 @@ def reversedL(l1):
         current_node = next_node
     
     return l1.tail, l1.head
+
+def reorderbyposition(l1):
+
+    odd_node = l1.head
+    even_node = l1.head.next
+
+    head_even_nodes = even_node
+
+    while even_node:
+        
+        print("odd none : ", odd_node)
+        print("even none: "), even_node
+        print("even_node.next :", odd_node.next)
+        odd_node.next = even_node.next
+        odd_node = odd_node.next
+
+        even_node.next = odd_node.next
+        
+        if even_node.next is None:
+            new_tail = even_node
+        even_node = even_node.next
+
+        odd_node.next = head_even_nodes
+
